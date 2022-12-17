eb8d499bf68d784989d15034005663379d9368fe
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2022-12-17 20:34:31 +0500

    Add files via upload

--- Demo.cs
+++ Demo.cs
@@ -13,28 +13,26 @@ void showDemo(double fps = 25) {
 
     // Picture
     Console.Write("\x1b[0;0m\x1b[H\x1b[2J\x1b[3J");
-    while (true)
-    {
-        Draw.drawRectangle('@', clrLBlue , 0, 0, termX, termY);
-        Draw.drawRectangle('g', clrGreen , 0, termY-7, termX, 7);
-        Draw.drawRectangle('h', clrOrange, termX-23, termY-13, 21, 10);
-        Draw.drawRectangle('d', clrRed   , termX-21, termY-9, 5, 6);
-        Draw.drawRectangle('h', clrYellow, termX-18, termY-7, 1, 2);
-        Draw.drawRectangle('w', clrYellow, termX-14, termY-10, 10, 5);
-        Draw.drawRectangle('f', clrOrange, termX-10, termY-10, 2, 5);
-        Draw.drawRectangle('f', clrOrange, termX-14, termY-8, 10, 1);
-        Draw.drawLine     ('r', clrRed   , termX-24, termY-12, termX-13, termY-18);
-        Draw.drawLine     ('r', clrRed   , termX-13, termY-18, termX-2, termY-12);
-        Draw.drawLine     ('r', clrRed   , termX-24, termY-12, termX-2, termY-12);
-        Draw.drawLine     ('r', clrRed   , termX-21, termY-13, termX-5, termY-13);
-        Draw.drawLine     ('r', clrRed   , termX-19, termY-14, termX-7, termY-14);
-        Draw.drawLine     ('r', clrRed   , termX-17, termY-15, termX-9, termY-15);
-        Draw.drawLine     ('r', clrRed   , termX-15, termY-16, termX-11, termY-16);
-        Draw.drawPixel    ('r', clrRed   , termX-13, termY-17);
-        Draw.drawEllipse  ('l', clrDBlue , 20, termY-4, 15, 2, true);
-        Draw.drawEllipse  ('s', clrYellow, 16, 7, 10, 5, true);
-        Thread.Sleep((int)(1000/fps));
-    }
+    string pic = "";
+    pic += new Rectangle('@', clrLBlue , 0, 0, termX, termY);
+    pic += new Rectangle('g', clrGreen , 0, termY-7, termX, 7);
+    pic += new Rectangle('h', clrOrange, termX-23, termY-13, 21, 10);
+    pic += new Rectangle('d', clrRed   , termX-21, termY-9, 5, 6);
+    pic += new Rectangle('h', clrYellow, termX-18, termY-7, 1, 2);
+    pic += new Rectangle('w', clrYellow, termX-14, termY-10, 10, 5);
+    pic += new Rectangle('f', clrOrange, termX-10, termY-10, 2, 5);
+    pic += new Rectangle('f', clrOrange, termX-14, termY-8, 10, 1);
+    pic += new Line     ('r', clrRed   , termX-24, termY-12, termX-13, termY-18);
+    pic += new Line     ('r', clrRed   , termX-13, termY-18, termX-2, termY-12);
+    pic += new Line     ('r', clrRed   , termX-24, termY-12, termX-2, termY-12);
+    pic += new Line     ('r', clrRed   , termX-21, termY-13, termX-5, termY-13);
+    pic += new Line     ('r', clrRed   , termX-19, termY-14, termX-7, termY-14);
+    pic += new Line     ('r', clrRed   , termX-17, termY-15, termX-9, termY-15);
+    pic += new Line     ('r', clrRed   , termX-15, termY-16, termX-11, termY-16);
+    pic += new Pixel    ('r', clrRed   , termX-13, termY-17);
+    pic += new Ellipse  ('l', clrDBlue , 20, termY-4, 15, 2, true);
+    pic += new Ellipse  ('s', clrYellow, 16, 7, 10, 5, true);
+    while (true) { Console.Write(pic); Thread.Sleep((int)(1000/fps)); }
 }
 
 showDemo();
--- Draw.cs
+++ Draw.cs
@@ -1,44 +1,119 @@
-﻿static partial class Draw
+public class Color 
 {
-    public static string doColor(int r = 255, int g = 255, int b = 255, string hex = "")
+    public Color(string hex, int r, int g, int b)
     {
-        if (hex.Length >= 7)
+        red = r;
+        green = g;
+        blue = b;
+        hexCode = hex;
+        if (hexCode.Length >= 7)
         {
-            hex = hex.Replace("#", "");
-            r = Convert.ToInt32($"0x{hex[0]}{hex[1]}", 16);
-            g = Convert.ToInt32($"0x{hex[2]}{hex[3]}", 16);
-            b = Convert.ToInt32($"0x{hex[4]}{hex[5]}", 16);
+            hexCode = hexCode.Replace("#", "");
+            red = Convert.ToInt32($"0x{hexCode[0]}{hexCode[1]}", 16);
+            green = Convert.ToInt32($"0x{hexCode[2]}{hexCode[3]}", 16);
+            blue = Convert.ToInt32($"0x{hexCode[4]}{hexCode[5]}", 16);
         }
-        return $"\x1b[38;2;{r};{g};{b}m";
     }
-    public static void drawPixel(char sym, string colorString, int posX, int posY)
+
+    public int red { get; set; }
+    public int green { get; set; }
+    public int blue { get; set; }
+    public string hexCode { get; set; }
+
+    override public string ToString()
+    {
+        return $"\x1b[38;2;{red};{green};{blue}m";
+    }
+}
+
+public class Pixel
+{
+    public Pixel(char psym, string pcolorString, int pposX, int pposY)
+    {
+        sym = psym;
+        colorString = pcolorString;
+        posX = pposX;
+        posY = pposY;
+    }
+
+    public char sym { get; set; }
+    public string colorString { get; set; }
+    public int posX { get; set; }
+    public int posY { get; set; }
+
+    override public string ToString()
+    {
+        return $"{colorString}\x1b[{posY};{posX}H{sym}";
+    }
+    public void draw()
     {
         Console.Write($"{colorString}\x1b[{posY};{posX}H{sym}");
     }
+}
 
-    public static void drawRectangle(char sym, string colorString, int posX, int posY, int geomX, int geomY)
+public class Rectangle
+{
+    public Rectangle(char rsym, string rcolorString, int rposX, int rposY, int rgeomX, int rgeomY)
+    {
+        sym = rsym;
+        colorString = rcolorString;
+        posX = rposX;
+        posY = rposY;
+        geomX = rgeomX;
+        geomY = rgeomY;
+    }
+
+    public char sym { get; set; }
+    public string colorString { get; set; }
+    public int posX { get; set; }
+    public int posY { get; set; }
+    public int geomX { get; set; }
+    public int geomY { get; set; }
+
+    public void draw()
     {
         string line = new string(sym, geomX);
         string rect = colorString;
         for (int gy = 0; gy < geomY; gy++)
         {
-            rect += $"\x1b[{posY + gy};{posX}H{line}"; 
+            rect += $"\x1b[{posY + gy};{posX}H{line}";
         }
         Console.Write(rect);
     }
-
-    public static void drawEllipse(char sym, string colorString, int posX, int posY, int radX, int radY, bool fill)
+    override public string ToString()
     {
+        string line = new string(sym, geomX);
+        string rect = colorString;
+        for (int gy = 0; gy < geomY; gy++)
+        {
+            rect += $"\x1b[{posY + gy};{posX}H{line}";
+        }
+        return rect;
+    }
+}
+
+public class Ellipse
+{
+    public Ellipse(char esym, string ecolorString, int eposX, int eposY, int eradX, int eradY, bool efill)
+    {
+        sym = esym;
+        colorString = ecolorString;
+        posX = eposX;
+        posY = eposY;
+        radX = eradX;
+        radY = eradY;
+        fill = efill;
+        ellipse = "";
         double dx, dy, d1, d2, x, y, powRadX, powRadY;
         powRadX = radX * radX;
         powRadY = radY * radY;
         
-        if (fill)
-        {
+        if (fill) {
             for(y=-radY; y<=radY; y++) {
                 for(x=-radX; x<=radX; x++) {
-                    if(x*x*radY*radY+y*y*radX*radX <= radY*radY*radX*radX)
-                        drawPixel(sym, colorString, (int)(posX + x), (int)(posY + y));
+                    if (x*x*radY*radY+y*y*radX*radX <= radY*radY*radX*radX) {
+                        ellipse += new Pixel(sym, colorString, (int)(posX + x), (int)(posY + y));
+                    }
                 }
             }
         }
@@ -51,10 +126,10 @@
 
         while (dx < dy)
         {
-            drawPixel(sym, colorString, (int)(x + posX), (int)(y + posY));
-            drawPixel(sym, colorString, (int)(-x + posX), (int)(y + posY));
-            drawPixel(sym, colorString, (int)(x + posX), (int)(-y + posY));
-            drawPixel(sym, colorString, (int)(-x + posX), (int)(-y + posY));
+            ellipse += new Pixel(sym, colorString, (int)(x + posX), (int)(y + posY));
+            ellipse += new Pixel(sym, colorString, (int)(-x + posX), (int)(y + posY));
+            ellipse += new Pixel(sym, colorString, (int)(x + posX), (int)(-y + posY));
+            ellipse += new Pixel(sym, colorString, (int)(-x + posX), (int)(-y + posY));
             if (d1 < 0)
             {
                 x++;
@@ -73,10 +148,10 @@
         d2 = powRadY * Math.Pow(x + 0.5f, 2) - powRadX * Math.Pow(y - 1, 2) - powRadX * powRadY;
         while (y >= 0)
         {
-            drawPixel(sym, colorString, (int)(x + posX), (int)(y + posY));
-            drawPixel(sym, colorString, (int)(-x + posX), (int)(y + posY));
-            drawPixel(sym, colorString, (int)(x + posX), (int)(-y + posY));
-            drawPixel(sym, colorString, (int)(-x + posX), (int)(-y + posY));
+            ellipse += new Pixel(sym, colorString, (int)(x + posX), (int)(y + posY));
+            ellipse += new Pixel(sym, colorString, (int)(-x + posX), (int)(y + posY));
+            ellipse += new Pixel(sym, colorString, (int)(x + posX), (int)(-y + posY));
+            ellipse += new Pixel(sym, colorString, (int)(-x + posX), (int)(-y + posY));
             if (d2 > 0)
             {
                 y--;
@@ -94,19 +169,67 @@
         }
     }
 
-    public static void drawLine(char sym, string colorString, int x0, int y0, int x1, int y1)
+    public char sym { get; set; }
+    public string colorString { get; set; }
+    public int posX { get; set; }
+    public int posY { get; set; }
+    public int radX { get; set; }
+    public int radY { get; set; }
+    public bool fill { get; set; }
+    public string ellipse;
+    public void draw()
+    {
+        Console.Write(ellipse);
+    }
+    override public string ToString()
+    {
+        return ellipse;
+    }
+}
+
+public class Line
+{
+    public Line(char lsym, string lcolorString, int lx0, int ly0, int lx1, int ly1)
+    {
+        sym = lsym;
+        colorString = lcolorString;
+        x0 = lx0;
+        y0 = ly0;
+        x1 = lx1;
+        y1 = ly1;
+    }
+
+    public char sym { get; set; }
+    public string colorString { get; set; }
+    public int x0 { get; set; }
+    public int y0 { get; set; }
+    public int x1 { get; set; }
+    public int y1 { get; set; }
+
+    override public string ToString()
     {
         int x, y, dX, dY, error, steepY;
         bool steep;
-        void Swap<T>(ref T Lhs, ref T Rhs)
-        {
-            T temp = Lhs;
-            Lhs = Rhs;
-            Rhs = temp;
-        }
+        string line = "";
         steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
-        if (steep) { Swap(ref x0, ref y0); Swap(ref x1, ref y1); }
-        if (x0 > x1) { Swap(ref x0, ref x1); Swap(ref y0, ref y1); }
+        if (steep) 
+        {
+            var temp = x0;
+            x0 = y0;
+            y0 = temp;
+            temp = x1;
+            x1 = y0;
+            y0 = temp;
+        }
+        if (x0 > x1) 
+        {
+            var temp = x0;
+            x0 = y0;
+            y0 = temp;
+            temp = x1;
+            x1 = y0;
+            y0 = temp;
+        }
         dX = x1 - x0;
         dY = Math.Abs(y1 - y0);
         steepY = y0 < y1 ? 1 : -1;
@@ -114,7 +237,7 @@
         error = dX / 2;
         for (x = x0; x <= x1; x++)
         {
-            drawPixel(sym, colorString, steep ? y : x, steep ? x : y);
+            line += new Pixel(sym, colorString, steep ? y : x, steep ? x : y);
             error -= dY;
             if (error < 0)
             {
@@ -122,5 +245,50 @@
                 error += dX;
             }
         }
+        return line;
+    }
+    public void draw()
+    {
+        int x, y, dX, dY, error, steepY;
+        bool steep;
+        Pixel pix = new Pixel(sym, colorString, 0,0);
+        string line = "";
+        steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+        if (steep) 
+        {
+            var temp = x0;
+            x0 = y0;
+            y0 = temp;
+            temp = x1;
+            x1 = y0;
+            y0 = temp;
+        }
+        if (x0 > x1) 
+        {
+            var temp = x0;
+            x0 = y0;
+            y0 = temp;
+            temp = x1;
+            x1 = y0;
+            y0 = temp;
+        }
+        dX = x1 - x0;
+        dY = Math.Abs(y1 - y0);
+        steepY = y0 < y1 ? 1 : -1;
+        y = y0;
+        error = dX / 2;
+        for (x = x0; x <= x1; x++)
+        {
+            pix.posX = steep ? y : x;
+            pix.posY = steep ? x : y;
+            line += pix;
+            error -= dY;
+            if (error < 0)
+            {
+                y += steepY;
+                error += dX;
+            }
+        }
+    Console.Write(line);
     }
 }

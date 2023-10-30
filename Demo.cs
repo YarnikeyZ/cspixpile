5532ff423035fee9b7c5164f278a180cbcdc2a79
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2023-10-31 00:56:46 +0500

    Add files via upload

--- Demo.cs
+++ Demo.cs
@@ -1,42 +1,47 @@
+using static CSpixpile;
+
 void showDemo(double fps = 25) {
     // Terminal size
     (int X, int Y) term = (Console.WindowWidth, Console.WindowHeight);
 
     // Colors
-    string clrRed    = new Color("#ff0000", (0,0,0)).ToString();
-    string clrLBlue  = new Color("#13a2e9", (0,0,0)).ToString();
-    string clrDBlue  = new Color("#0000ff", (0,0,0)).ToString();
-    string clrGreen  = new Color("#0cd205", (0,0,0)).ToString();
-    string clrOrange = new Color("#de8500", (0,0,0)).ToString();
-    string clrYellow = new Color("#ffff00", (0,0,0)).ToString();
+    Color clrBlack  = new ("#000000", (0, 0, 0));
+    Color clrRed    = new ("#ff0000", (0, 0, 0));
+    Color clrLBlue  = new ("#13a2e9", (0, 0, 0));
+    Color clrDBlue  = new ("#0000ff", (0, 0, 0));
+    Color clrGreen  = new ("#0cd205", (0, 0, 0));
+    Color clrOrange = new ("#de8500", (0, 0, 0));
+    Color clrYellow = new ("#ffff00", (0, 0, 0));
 
     // Picture
     Console.Write(DrawGlobals.CLR);
     string pic = "";
 
-    pic += new Rectangle('@', clrLBlue , (0, 0), (term.X, term.Y), true);
-    pic += new Rectangle('g', clrGreen , (0, term.Y-7), (term.X, 7), true);
+    pic += new Rectangle('@', clrLBlue , clrLBlue , (0, 0), (term.X, term.Y), true);
+    pic += new Rectangle('g', clrGreen , clrGreen , (0, term.Y-7), (term.X, 7), true);
 
-    pic += new Rectangle('h', clrOrange, (term.X-23, term.Y-13), (21, 10), true);
-    pic += new Rectangle('d', clrRed   , (term.X-21, term.Y-9 ), (5 , 6 ), true);
-    pic += new Rectangle('h', clrYellow, (term.X-18, term.Y-7 ), (1 , 2 ), true);
-    pic += new Rectangle('w', clrYellow, (term.X-14, term.Y-10), (10, 5 ), true);
-    pic += new Rectangle('f', clrOrange, (term.X-10, term.Y-10), (2 , 5 ), true);
-    pic += new Rectangle('f', clrOrange, (term.X-14, term.Y-8 ), (10, 1 ), true);
+    pic += new Rectangle('h', clrOrange, clrOrange, (term.X-23, term.Y-13), (21, 10), true);
+    pic += new Rectangle('d', clrRed   , clrRed   , (term.X-21, term.Y-9 ), (5 , 6 ), true);
+    pic += new Rectangle('h', clrYellow, clrYellow, (term.X-18, term.Y-7 ), (1 , 2 ), true);
+    pic += new Rectangle('w', clrYellow, clrYellow, (term.X-14, term.Y-10), (10, 5 ), true);
+    pic += new Rectangle('f', clrOrange, clrOrange, (term.X-10, term.Y-10), (2 , 5 ), true);
+    pic += new Rectangle('f', clrOrange, clrOrange, (term.X-14, term.Y-8 ), (10, 1 ), true);
 
-    pic += new Line     ('r', clrRed   , (term.X-24, term.Y-12), (term.X-13, term.Y-18));
-    pic += new Line     ('r', clrRed   , (term.X-13, term.Y-18), (term.X-2 , term.Y-12));
-    pic += new Line     ('r', clrRed   , (term.X-24, term.Y-12), (term.X-2 , term.Y-12));
-    pic += new Line     ('r', clrRed   , (term.X-21, term.Y-13), (term.X-5 , term.Y-13));
-    pic += new Line     ('r', clrRed   , (term.X-19, term.Y-14), (term.X-7 , term.Y-14));
-    pic += new Line     ('r', clrRed   , (term.X-17, term.Y-15), (term.X-9 , term.Y-15));
-    pic += new Line     ('r', clrRed   , (term.X-15, term.Y-16), (term.X-11, term.Y-16));
+    pic += new Line     ('r', clrRed   , clrRed   , (term.X-24, term.Y-12), (term.X-13, term.Y-18));
+    pic += new Line     ('r', clrRed   , clrRed   , (term.X-13, term.Y-18), (term.X-2 , term.Y-12));
+    pic += new Line     ('r', clrRed   , clrRed   , (term.X-24, term.Y-12), (term.X-2 , term.Y-12));
+    pic += new Line     ('r', clrRed   , clrRed   , (term.X-21, term.Y-13), (term.X-5 , term.Y-13));
+    pic += new Line     ('r', clrRed   , clrRed   , (term.X-19, term.Y-14), (term.X-7 , term.Y-14));
+    pic += new Line     ('r', clrRed   , clrRed   , (term.X-17, term.Y-15), (term.X-9 , term.Y-15));
+    pic += new Line     ('r', clrRed   , clrRed   , (term.X-15, term.Y-16), (term.X-11, term.Y-16));
 
-    pic += new Pixel    ('r', clrRed   , (term.X-13, term.Y-17));
+    pic += new Pixel    ('r', clrRed   , clrRed   , (term.X-13, term.Y-17));
 
-    pic += new Ellipse  ('l', clrDBlue , (20, term.Y-4), (15, 2), true);
-    pic += new Ellipse  ('s', clrYellow, (16, 7), (10, 5), true);
-
+    pic += new Ellipse  ('l', clrDBlue , clrDBlue , (20, term.Y-4), (15, 2), true);
+    pic += new Ellipse  ('s', clrYellow, clrYellow, (16, 7), (10, 5), true);
+    
+    pic += DrawGlobals.CCLR+MoveCursor(term);
+    
     // Output
     if (fps > 0)
     {
--- Draw.cs
+++ Draw.cs
@@ -1,277 +1,324 @@
-public static class DrawGlobals
+public class CSpixpile
 {
-    public static string ESC = "\x1b";
-    public static string CCLR = $"{ESC}[0;0m";
-    public static string CLR = $"{CCLR}{ESC}[H{ESC}[2J{ESC}[3J";
-}
-
-public class Color 
-{
-    public Color(string _hex, (int R, int G, int B) _rgb)
+    public static string ColorThem(Color foreground, Color background, bool draw = true)
     {
-        rgb = _rgb;
-        hex = _hex;
-        color = "";
-        int R = _rgb.R;
-        int G = _rgb.G;
-        int B = _rgb.B;
-        if (hex.Length >= 7)
+        string them = $"{DrawGlobals.ESC}[38;2;{foreground.rgb.R};{foreground.rgb.G};{foreground.rgb.B}m{DrawGlobals.ESC}[48;2;{background.rgb.R};{background.rgb.G};{background.rgb.B}m";
+        if (draw) { Console.Write(them); }
+        return them;
+    }
+
+    public static string ColorIt(string text, Color foreground, Color background, bool draw = true)
+    {
+        string it = $"{DrawGlobals.ESC}[38;2;{foreground.rgb.R};{foreground.rgb.G};{foreground.rgb.B}m{DrawGlobals.ESC}[48;2;{background.rgb.R};{background.rgb.G};{background.rgb.B}m{text}{DrawGlobals.CCLR}";
+        if (draw) { Console.Write(it); }
+        return it;
+    }
+
+    public static string MoveCursor((int X, int Y) pos, bool draw = true)
+    {
+        string cursor = $"{DrawGlobals.ESC}[{pos.Y};{pos.X}H";
+        if (draw) { Console.Write(cursor); }
+        return cursor;
+    }
+
+    public static (bool top, bool bottom, bool left, bool right) CheckCollision(Rectangle rect1, Rectangle rect2)
+    {
+        (bool top, bool bottom, bool left, bool right) collision = (false, false, false, false);
+        if (rect1.pos.Y >= rect2.pos.Y)                               { collision.top    = true; }
+        if (rect1.pos.Y + rect1.size.Y >= rect2.pos.Y + rect2.size.Y) { collision.bottom = true; }
+        if (rect1.pos.X >= rect2.pos.X)                               { collision.left   = true; }
+        if (rect1.pos.X + rect1.size.X >= rect2.pos.X + rect2.size.X) { collision.bottom = true; }
+        return collision;
+    }
+
+    public static class DrawGlobals
+    {
+        public static string ESC = "\x1b";
+        public static string CCLR = $"{ESC}[0;0m";
+        public static string CLR = $"{CCLR}{ESC}[H{ESC}[2J{ESC}[3J";
+    }
+
+    public class Color
+    {
+        public Color(string _hex, (int R, int G, int B) _rgb)
         {
-            hex = hex.Replace("#", "");
-            R = Convert.ToInt32($"0x{hex[0]}{hex[1]}", 16);
-            G = Convert.ToInt32($"0x{hex[2]}{hex[3]}", 16);
-            B = Convert.ToInt32($"0x{hex[4]}{hex[5]}", 16);
-        }
-        rgb = (R, G, B);
-        color = $"{DrawGlobals.ESC}[38;2;{rgb.R};{rgb.G};{rgb.B}m";
-    }
+            rgb = _rgb;
+            hex = _hex;
+            color = "";
 
-    public (int R, int G, int B) rgb { get; set; }
-    public string hex { get; set; }
-    public string color;
-
-    override public string ToString()
-    {
-        return color;
-    }
-}
-
-public class Pixel
-{
-    public Pixel(char _sym, string _colorString, (int _X, int _Y) _pos)
-    {
-        sym = _sym;
-        colorString = _colorString;
-        pos = _pos;
-        pixel = $"{colorString}{DrawGlobals.ESC}[{pos.Y};{pos.X}H{sym}";
-    }
-
-    public char sym { get; set; }
-    public string colorString { get; set; }
-    public (int X, int Y) pos { get; set; }
-    public string pixel;
-
-    public void draw()
-    {
-        Console.Write(pixel);
-    }
-
-    override public string ToString()
-    {
-        return pixel;
-    }
-}
-
-public class Rectangle
-{
-    public Rectangle(char _sym, string _colorString, (int _X, int _Y) _pos, (int _X, int _Y) _size, bool _fill)
-    {
-        sym = _sym;
-        colorString = _colorString;
-        pos = _pos;
-        size = _size;
-        fill = _fill;
-
-        rect = colorString;
-        string line = new string(sym, size.X);
-
-        if (fill)
-        {
-            for (int gy = 0; gy < size.Y; gy++)
+            int R = _rgb.R;
+            int G = _rgb.G;
+            int B = _rgb.B;
+            if (hex.Length >= 7)
             {
-                rect += $"{DrawGlobals.ESC}[{pos.Y + gy};{pos.X}H{line}";
+                hex = hex.Replace("#", "");
+                R = Convert.ToInt32($"0x{hex[0]}{hex[1]}", 16);
+                G = Convert.ToInt32($"0x{hex[2]}{hex[3]}", 16);
+                B = Convert.ToInt32($"0x{hex[4]}{hex[5]}", 16);
+            }
+            rgb = (R, G, B);
+        }
+
+
+        public (int R, int G, int B) rgb { get; set; }
+        public string hex { get; set; }
+        public string color;
+
+        override public string ToString()
+        {
+            return color;
+        }
+    }
+
+    public class Pixel
+    {
+        public Pixel(char _sym, Color _colorForeground, Color _colorBackground, (int _X, int _Y) _pos)
+        {
+            sym = _sym;
+            colorForeground = _colorForeground;
+            colorBackground = _colorBackground;
+            pos = _pos;
+            pixel = ColorIt($"{MoveCursor((pos.X, pos.Y), false)}{sym}", colorForeground, colorBackground, false);
+        }
+
+        public char sym { get; set; }
+        public Color colorForeground { get; set; }
+        public Color colorBackground { get; set; }
+        public (int X, int Y) pos { get; set; }
+        public string pixel;
+
+        public void draw()
+        {
+            Console.Write(pixel);
+        }
+
+        override public string ToString()
+        {
+            return pixel;
+        }
+    }
+
+    public class Rectangle
+    {
+        public Rectangle(char _sym, Color _colorForeground, Color _colorBackground, (int _X, int _Y) _pos, (int _X, int _Y) _size, bool _fill)
+        {
+            sym = _sym;
+            colorForeground = _colorForeground;
+            colorBackground = _colorBackground;
+            pos = _pos;
+            size = _size;
+            fill = _fill;
+
+            rect = ColorThem(colorForeground, colorBackground, false);
+            string line = new string(sym, size.X);
+
+            if (fill)
+            {
+                for (int gy = 0; gy < size.Y; gy++)
+                {
+                    rect += $"{MoveCursor((pos.X, pos.Y + gy), false)}{line}";
+                }
+            }
+            else
+            {
+                rect += $"{MoveCursor((pos.X, pos.Y), false)}{line}";
+                for (int gy = 0; gy < size.Y; gy++)
+                {
+                    rect += $"{MoveCursor((pos.X, pos.Y + gy), false)}{sym}{MoveCursor((pos.X + size.X - 1, pos.Y + gy), false)}{sym}";
+                }
+                rect += $"{MoveCursor((pos.X, pos.Y + size.Y - 1), false)}{line}";
             }
         }
-        else
+
+        public char sym { get; set; }
+        public Color colorForeground { get; set; }
+        public Color colorBackground { get; set; }
+        public (int X, int Y) pos { get; set; }
+        public (int X, int Y) size { get; set; }
+        public bool fill { get; set; }
+        public string rect;
+
+        public void draw()
         {
-            rect += $"{DrawGlobals.ESC}[{pos.Y};{pos.X}H{line}";
-            for (int gy = 0; gy < size.Y; gy++)
-            {
-                rect += $"{DrawGlobals.ESC}[{pos.Y + gy};{pos.X}H{sym}{DrawGlobals.ESC}[{pos.Y + gy};{pos.X+size.X-1}H{sym}";
-            }
-            rect += $"{DrawGlobals.ESC}[{pos.Y+size.Y-1};{pos.X}H{line}";
+            Console.Write(rect);
+        }
+
+        override public string ToString()
+        {
+            return rect;
         }
     }
 
-    public char sym { get; set; }
-    public string colorString { get; set; }
-    public (int X, int Y) pos { get; set; }
-    public (int X, int Y) size { get; set; }
-    public bool fill { get; set; }
-    public string rect;
-
-    public void draw()
+    public class Ellipse
     {
-        Console.Write(rect);
-    }
+        public Ellipse(char _sym, Color _colorForeground, Color _colorBackground, (int _X, int _Y) _pos, (int _X, int _Y) _rad, bool _fill)
+        {
+            sym = _sym;
+            colorForeground = _colorForeground;
+            colorBackground = _colorBackground;
+            pos = _pos;
+            rad = _rad;
+            fill = _fill;
+            ellipse = "";
+            int posX = pos.X;
+            int posY = pos.Y;
+            int radX = rad.X;
+            int radY = rad.Y;
+            double dx, dy, d1, d2, x, y, powRadX, powRadY;
+            powRadX = radX * radX;
+            powRadY = radY * radY;
 
-    override public string ToString()
-    {
-        return rect;
-    }
-}
-
-public class Ellipse
-{
-    public Ellipse(char _sym, string _colorString, (int _X, int _Y) _pos, (int _X, int _Y) _rad, bool _fill)
-    {
-        sym = _sym;
-        colorString = _colorString;
-        pos = _pos;
-        rad = _rad;
-        fill = _fill;
-        ellipse = "";
-        int posX = pos.X;
-        int posY = pos.Y;
-        int radX = rad.X;
-        int radY = rad.Y;
-        double dx, dy, d1, d2, x, y, powRadX, powRadY;
-        powRadX = radX * radX;
-        powRadY = radY * radY;
-        
-        if (fill) {
-            for(y=-radY; y<=radY; y++) {
-                for(x=-radX; x<=radX; x++) {
-                    if (x*x*radY*radY+y*y*radX*radX <= radY*radY*radX*radX) {
-                        ellipse += new Pixel(sym, colorString, ((int)(posX + x), (int)(posY + y)));
+            if (fill)
+            {
+                for (y = -radY; y <= radY; y++)
+                {
+                    for (x = -radX; x <= radX; x++)
+                    {
+                        if (x * x * radY * radY + y * y * radX * radX <= radY * radY * radX * radX)
+                        {
+                            ellipse += new Pixel(sym, colorForeground, colorBackground, ((int)(posX + x), (int)(posY + y)));
+                        }
                     }
                 }
             }
-        }
-        
-        x = 0;
-        y = radY;
-        d1 = powRadY - powRadX * radY + 0.25f * powRadX;
-        dx = 2 * powRadY * x;
-        dy = 2 * powRadX * y;
 
-        while (dx < dy)
-        {
-            ellipse += new Pixel(sym, colorString, ((int)(x + posX), (int)(y + posY)));
-            ellipse += new Pixel(sym, colorString, ((int)(-x + posX), (int)(y + posY)));
-            ellipse += new Pixel(sym, colorString, ((int)(x + posX), (int)(-y + posY)));
-            ellipse += new Pixel(sym, colorString, ((int)(-x + posX), (int)(-y + posY)));
-            if (d1 < 0)
+            x = 0;
+            y = radY;
+            d1 = powRadY - powRadX * radY + 0.25f * powRadX;
+            dx = 2 * powRadY * x;
+            dy = 2 * powRadX * y;
+
+            while (dx < dy)
             {
-                x++;
-                dx = dx + 2 * powRadY;
-                d1 = d1 + dx + powRadY;
+                ellipse += new Pixel(sym, colorForeground, colorBackground, ((int)(x + posX), (int)(y + posY)));
+                ellipse += new Pixel(sym, colorForeground, colorBackground, ((int)(-x + posX), (int)(y + posY)));
+                ellipse += new Pixel(sym, colorForeground, colorBackground, ((int)(x + posX), (int)(-y + posY)));
+                ellipse += new Pixel(sym, colorForeground, colorBackground, ((int)(-x + posX), (int)(-y + posY)));
+                if (d1 < 0)
+                {
+                    x++;
+                    dx = dx + 2 * powRadY;
+                    d1 = d1 + dx + powRadY;
+                }
+                else
+                {
+                    x++;
+                    y--;
+                    dx = dx + 2 * powRadY;
+                    dy = dy - 2 * powRadX;
+                    d1 = d1 + dx - dy + powRadY;
+                }
             }
-            else
+            d2 = powRadY * Math.Pow(x + 0.5f, 2) - powRadX * Math.Pow(y - 1, 2) - powRadX * powRadY;
+            while (y >= 0)
             {
-                x++;
-                y--;
-                dx = dx + 2 * powRadY;
-                dy = dy - 2 * powRadX;
-                d1 = d1 + dx - dy + powRadY;
+                ellipse += new Pixel(sym, colorForeground, colorBackground, ((int)(x + posX), (int)(y + posY)));
+                ellipse += new Pixel(sym, colorForeground, colorBackground, ((int)(-x + posX), (int)(y + posY)));
+                ellipse += new Pixel(sym, colorForeground, colorBackground, ((int)(x + posX), (int)(-y + posY)));
+                ellipse += new Pixel(sym, colorForeground, colorBackground, ((int)(-x + posX), (int)(-y + posY)));
+                if (d2 > 0)
+                {
+                    y--;
+                    dy = dy - 2 * powRadX;
+                    d2 = d2 + powRadX - dy;
+                }
+                else
+                {
+                    y--;
+                    x++;
+                    dx = dx + 2 * powRadY;
+                    dy = dy - 2 * powRadX;
+                    d2 = d2 + dx - dy + powRadX;
+                }
             }
         }
-        d2 = powRadY * Math.Pow(x + 0.5f, 2) - powRadX * Math.Pow(y - 1, 2) - powRadX * powRadY;
-        while (y >= 0)
+
+        public char sym { get; set; }
+        public Color colorForeground { get; set; }
+        public Color colorBackground { get; set; }
+        public (int X, int Y) pos { get; set; }
+        public (int X, int Y) rad { get; set; }
+        public bool fill { get; set; }
+        public string ellipse;
+        public void draw()
         {
-            ellipse += new Pixel(sym, colorString, ((int)(x + posX), (int)(y + posY)));
-            ellipse += new Pixel(sym, colorString, ((int)(-x + posX), (int)(y + posY)));
-            ellipse += new Pixel(sym, colorString, ((int)(x + posX), (int)(-y + posY)));
-            ellipse += new Pixel(sym, colorString, ((int)(-x + posX), (int)(-y + posY)));
-            if (d2 > 0)
-            {
-                y--;
-                dy = dy - 2 * powRadX;
-                d2 = d2 + powRadX - dy;
-            }
-            else
-            {
-                y--;
-                x++;
-                dx = dx + 2 * powRadY;
-                dy = dy - 2 * powRadX;
-                d2 = d2 + dx - dy + powRadX;
-            }
+            Console.Write(ellipse);
+        }
+        override public string ToString()
+        {
+            return ellipse;
         }
     }
 
-    public char sym { get; set; }
-    public string colorString { get; set; }
-    public (int X, int Y) pos { get; set; }
-    public (int X, int Y) rad { get; set; }
-    public bool fill { get; set; }
-    public string ellipse;
-    public void draw()
+    public class Line
     {
-        Console.Write(ellipse);
-    }
-    override public string ToString()
-    {
-        return ellipse;
-    }
-}
+        public Line(char _sym, Color _colorForeground, Color _colorBackground, (int _X, int _Y) _pos1, (int _X, int _Y) _pos2)
+        {
+            sym = _sym;
+            colorForeground = _colorForeground;
+            colorBackground = _colorBackground;
+            pos1 = _pos1;
+            pos2 = _pos2;
+            line = "";
 
-public class Line
-{
-    public Line(char _sym, string _colorString, (int _X, int _Y) _pos1, (int _X, int _Y) _pos2)
-    {
-        sym = _sym;
-        colorString = _colorString;
-        pos1 = _pos1;
-        pos2 = _pos2;
-        line = "";
+            int pos1X = pos1.X;
+            int pos1Y = pos1.Y;
+            int pos2X = pos2.X;
+            int pos2Y = pos2.Y;
+            int x, y, dX, dY, error, steepY;
+            bool steep;
 
-        int pos1X = pos1.X;
-        int pos1Y = pos1.Y;
-        int pos2X = pos2.X;
-        int pos2Y = pos2.Y;
-        int x, y, dX, dY, error, steepY;
-        bool steep;
-        
-        steep = Math.Abs(pos2Y - pos1Y) > Math.Abs(pos2X - pos1X);
-        if (steep) 
-        {
-            var temp = pos1X;
-            pos1X = pos1Y;
-            pos1Y = temp;
-            temp = pos2X;
-            pos2X = pos1Y;
-            pos1Y = temp;
-        }
-        if (pos1X > pos2X) 
-        {
-            var temp = pos1X;
-            pos1X = pos1Y;
-            pos1Y = temp;
-            temp = pos2X;
-            pos2X = pos1Y;
-            pos1Y = temp;
-        }
-        dX = pos2X - pos1X;
-        dY = Math.Abs(pos2Y - pos1Y);
-        steepY = pos1Y < pos2Y ? 1 : -1;
-        y = pos1Y;
-        error = dX / 2;
-        for (x = pos1X; x <= pos2X; x++)
-        {
-            line += new Pixel(sym, colorString, (steep ? y : x, steep ? x : y));
-            error -= dY;
-            if (error < 0)
+            steep = Math.Abs(pos2Y - pos1Y) > Math.Abs(pos2X - pos1X);
+            if (steep)
             {
-                y += steepY;
-                error += dX;
+                var temp = pos1X;
+                pos1X = pos1Y;
+                pos1Y = temp;
+                temp = pos2X;
+                pos2X = pos1Y;
+                pos1Y = temp;
+            }
+            if (pos1X > pos2X)
+            {
+                var temp = pos1X;
+                pos1X = pos1Y;
+                pos1Y = temp;
+                temp = pos2X;
+                pos2X = pos1Y;
+                pos1Y = temp;
+            }
+            dX = pos2X - pos1X;
+            dY = Math.Abs(pos2Y - pos1Y);
+            steepY = pos1Y < pos2Y ? 1 : -1;
+            y = pos1Y;
+            error = dX / 2;
+            for (x = pos1X; x <= pos2X; x++)
+            {
+                line += new Pixel(sym, colorForeground, colorBackground, (steep ? y : x, steep ? x : y));
+                error -= dY;
+                if (error < 0)
+                {
+                    y += steepY;
+                    error += dX;
+                }
             }
         }
-    }
 
-    public char sym { get; set; }
-    public string colorString { get; set; }
-    public (int X, int Y) pos1 { get; set; }
-    public (int X, int Y) pos2 { get; set; }
-    public string line;
+        public char sym { get; set; }
+        public Color colorForeground { get; set; }
+        public Color colorBackground { get; set; }
+        public (int X, int Y) pos1 { get; set; }
+        public (int X, int Y) pos2 { get; set; }
+        public string line;
 
-    public void draw()
-    {
-        Console.Write(line);
-    }
+        public void draw()
+        {
+            Console.Write(line);
+        }
 
-    override public string ToString()
-    {
-        return line;
+        override public string ToString()
+        {
+            return line;
+        }
     }
 }
